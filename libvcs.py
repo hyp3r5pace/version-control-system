1b5399c3cbec3c0c7c0e648faf30671aaa37f9f9
Soumyajit Deb <debsoumyajit100@gmail.com>
2021-01-28 15:02:24 +0530

    Add control flow and declare methods for parsing command line arguments

--- libvcs.py
+++ libvcs.py
@@ -8,4 +8,27 @@ import sys
 import zlib
 import configparser
 
+# command line argument parsing
+argparser = argparse.ArgumentParser()
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
+
+
+def main(argv = sys.argv[1:]):
+    args = argparser.parse_args(argv)
+
+    if args.command == "add"                 : cmd_add(args)
+    elif args.command == "cat-file"            : cmd_cat_file(args)
+    elif args.command == "checkout"            : cmd_checkout(args)
+    elif args.command == "commit"              : cmd_commit(args)
+    elif args.command == "hash-object"         : cmd_hash_object(args)
+    elif args.command == "log"                 : cmd_log(args)
+    elif args.command == "init"                : cmd_init(args)
+    elif args.command == "ls-tree"             : cmd_ls_tree(args)
+    elif args.command == "merge"               : cmd_merge(args)
+    elif args.command == "rebase"              : cmd_rebase(args)
+    elif args.command == "rev-parse"           : cmd_rev_parse(args)
+    elif args.command == "rm"                  : cmd_rm(args)
+    elif args.command == "show-ref"            : cmd_show_ref(args)
+    elif args.command == "tag"                 : cmd_tag(args)
 
