5df5ef5316dd5b25eb247c3bae02d77c9509cf60
Soumyajit Deb <debsoumyajit100@gmail.com>
2021-02-01 11:53:22 +0530

    Add a vcs repository abstraction class

--- libvcs.py
+++ libvcs.py
@@ -8,6 +8,37 @@ import sys
 import zlib
 import configparser
 
+
+class vcsRepository(object):
+    """Abstraction of a vcs repository"""
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        #  Read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception("Unsupported repositoryformatversion %s" % vers)
+    
+
+
+
+
+
 # command line argument parsing
 argparser = argparse.ArgumentParser()
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
